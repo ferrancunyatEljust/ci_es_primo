97a85b24570ac4a4b1583028aab0d83d97b50cc7
Ferran Cunyat <ferrancunyat@ieseljust.com>
2022-03-31 23:57:38 +0200

    Refactorizamos la función es_primo para que obtenga un 10 con pylint

--- src/funciones.py
+++ src/funciones.py
@@ -1,10 +1,20 @@
+"""Módulo que contiene la función es_primo"""
 import math
 
 
-def es_primo(num):
-    if num < 2:
+def es_primo(entero):
+    """
+    Función para determinar si un número es primo:
+
+    Entrada:
+    - entero: número entero a determinar si es primo
+
+    Salida:
+    - True/False: en caso de ser primo o no
+    """
+    if entero < 2:
         return False
-    for n in range(2, math.floor(math.sqrt(num) + 1)):
-        if num % n == 0:
+    for factor in range(2, math.floor(math.sqrt(entero) + 1)):
+        if entero % factor == 0:
             return False
     return True
